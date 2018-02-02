3d8527a1f74570ab34a6ef94512342cdcb4f3d53
Karan <chunarakaran@gmail.com>
2018-02-02 14:01:17 +0530

    About Fragment Create And Put Old code into this application

--- app/src/main/java/com/exportershouse/expoapp/MainActivity.java
+++ app/src/main/java/com/exportershouse/expoapp/MainActivity.java
@@ -15,8 +15,6 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-
-
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
